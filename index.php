0eae0c38ec977510f0bcbb27c3df8f7a7aed3e6d
Jaron van Well <49275554+JaronVW@users.noreply.github.com>
2020-03-07 10:46:36 +0100

    base css

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/base_style.css">
     <title>jaronvw</title>
 
 </head>
@@ -26,11 +27,21 @@
             </ul>
         </nav>
     </header>
-    
+
     <main>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+            <h2>hallo</h2>
+            <article><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit consectetur animi illum nobis vitae impedit voluptatibus sed qui odio?</p></article>
+        </div>
+        <div>
+            <article></article>
+        </div>
+        <div>
+            <article></article>
+        </div>
     </main>
+    <footer>
+        
+    </footer>
 </body>
 </html>
